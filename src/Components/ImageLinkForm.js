2c44d0c2076a0f223161f0d43eaa65ec37e69d4a
kmdshojib <70483595+kmdshojib@users.noreply.github.com>
2021-09-26 15:32:44 +0600

    rooled back to previus version

--- src/Components/ImageLinkForm.js
+++ src/Components/ImageLinkForm.js
@@ -5,33 +5,23 @@ import './style/imageForm.css'
 const ImageLinkForm =({onInputChange,onButtonSubmit})=>{
     return(
         <div className='ma4 mt0'>
-            <p className="f3">
-                {'Copy and paste a image Link .! This Magic Brain will detect faces in your picture! '}
-            </p>
-           <div className='center '>
-                <div className='form center pa4 br3 shadow-3 ' >
-                    
-                    <input 
-                    type="text" 
-                    className="w-90 f4 pa2 center" 
-                    placeHolder="Enter a Image Url"
-                    style={{marginBottom:'1px'}} 
-                    onChange={(onInputChange)}
+        <p className="f3">
+            {'Copy and Paste Image Link: This Magic Brain will detect faces in your picture!'}
+        </p>
+        <div className='center'>
+            <div className='form center pa4 br3 shadow-3 ' >
+            <input 
+                type="tex" 
+                className="w-70 f4 pa2 center" 
+                onChange={(onInputChange)}
+                placeholder="Copy Image link from anywhere and Paste Image Link Here"
 
-                    />
-
-                    <div className="w-80">
-                    <button 
-                    type="submit" 
-                    className="w-30 grow f5 link ph3 pv2 dib white bg-light-purple btn"
-                    onClick={(onButtonSubmit)}
-                    >Detect</button>
-                    </div>
-                </div>
-
-              
+            />
+            <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={(onButtonSubmit)}>Detect</button>
             </div>
         </div>
+    </div>
     )
 }
 export default ImageLinkForm;
